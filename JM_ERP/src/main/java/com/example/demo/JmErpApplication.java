91d033b99f5b0175e3db9b3ceff50a095308c616
SEO <wnsgk5748@naver.com>
2024-01-17 16:31:27 +0900

    Beginning Project and Name Branchs

---
+++ JM_ERP/src/main/java/com/example/demo/JmErpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmErpApplication.class, args);
+	}
+
+}
