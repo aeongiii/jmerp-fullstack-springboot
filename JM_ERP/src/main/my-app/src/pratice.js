55e92d30f73aed8603813b959de1ecdd8aa116bd
studydev2023 <151710964+studydev2023@users.noreply.github.com>
2024-01-24 16:47:41 +0900

    0124

--- JM_ERP/src/main/my-app/src/pratice.js
+++
@@ -1,5 +0,0 @@
-  document.querySelector(".side-menu__icon").addEventListener("click", (e) => {
-  console.log(e.target);
-    e.target.classList.toggle("menu-on");
-    document.querySelector(".side-menu").classList.toggle("menu-on");
-});
--- JM_ERP/src/main/my-app/src/side--bar.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".side--bar--icon").addEventListener("click", (e) => {
-  console.log(e.target);
-    e.target.classList.toggle("menu-on");
-    document.querySelector(".side--bar").classList.toggle("menu-on");
-});
