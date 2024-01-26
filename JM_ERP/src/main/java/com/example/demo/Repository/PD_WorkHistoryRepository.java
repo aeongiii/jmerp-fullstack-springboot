f695e5acf9469197fdc7f07af6993c6a8bdddfdc
studydev2023 <151710964+studydev2023@users.noreply.github.com>
2024-01-26 16:59:52 +0900

    0126 초기세팅 완료

--- JM_ERP/src/main/java/com/example/demo/Repository/PD_WorkHistoryRepository.java
+++ JM_ERP/src/main/java/com/example/demo/Repository/PD_WorkHistoryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.Entity.PD_WorkHistory;
 
-public interface PD_WorkHistoryRepository extends JpaRepository<PD_WorkHistory, String>{
+public interface PD_WorkHistoryRepository extends JpaRepository<PD_WorkHistory, Integer>{
 
 }
