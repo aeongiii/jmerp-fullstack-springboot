1d3d14fefc4ef658fc01890804624e73cf0af921
skshrnrdj <skshrnrdj@gmail.com>
2024-01-26 16:51:05 +0900

    01/26

--- JM_ERP/src/main/java/com/example/demo/Controller/SlipController.java
+++ JM_ERP/src/main/java/com/example/demo/Controller/SlipController.java
@@ -1,7 +1,12 @@
 package com.example.demo.Controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.Entity.AC_saleSlip;
 import com.example.demo.Service.AC_purchaseSlipService;
 import com.example.demo.Service.AC_saleSlipService;
 
@@ -13,4 +18,11 @@ public class SlipController {
 	//전표관리를 당담하는 웹사이트
 	private final AC_saleSlipService saleSlipService;
 	private final AC_purchaseSlipService purchaseSlipService;
+	
+    @GetMapping("/saleslip")
+    public String search(Model model) {
+        List<AC_saleSlip> saleSlipList = this.saleSlipService.getList();
+        model.addAttribute("saleSlipList", saleSlipList);
+        return "saleSlip";
+    }
 }
--- JM_ERP/src/main/java/com/example/demo/Service/AC_saleSlipService.java
+++ JM_ERP/src/main/java/com/example/demo/Service/AC_saleSlipService.java
@@ -1,7 +1,10 @@
 package com.example.demo.Service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.demo.Entity.AC_saleSlip;
 import com.example.demo.Repository.AC_saleSlipRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +14,9 @@ import lombok.RequiredArgsConstructor;
 public class AC_saleSlipService {
 	
 	private final AC_saleSlipRepository saleSlipRepository;
+	
+    public List<AC_saleSlip> getList() {
+        return this.saleSlipRepository.findAll();
+    }
 
 }
