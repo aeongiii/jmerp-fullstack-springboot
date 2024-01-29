75326c4f022def26f6f3e5e3a2a39bb72aa3650f
aeongiii <151815152+aeongiii@users.noreply.github.com>
2024-01-29 14:39:29 +0900

    20240129 사원검색 기능 완료 (사원 관련 기능 모두 구현 완료)

--- JM_ERP/src/main/java/com/example/demo/Controller/HR_controller.java
+++ JM_ERP/src/main/java/com/example/demo/Controller/HR_controller.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.Entity.HR_dept;
 import com.example.demo.Entity.HR_mem;
@@ -20,22 +21,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 public class HR_controller {
-	
+
 	private final HR_memService memService;
 
-	
 	@GetMapping("")
 	public String HR() {
 		return "HR_main";
 	}
-	
+
 	@GetMapping("/mem/list")
 	public String list(Model model) {
 		List<HR_mem> memList = this.memService.getList();
 		model.addAttribute("memList", memList);
 		return "HR_memList";
 	}
-	
+
 	@GetMapping("/mem/create")
 	public String create(Model model) {
 		List<HR_dept> deptList = memService.getdeptList();
@@ -43,15 +43,22 @@ public class HR_controller {
 		model.addAttribute("HR_memCreateForm", new HR_memCreateForm()); // HR_memCreateForm 객체를 모델에 추가
 		return "HR_memCreate";
 	}
-	
+
 	@PostMapping("/mem/create")
 	public String create(@Valid HR_memCreateForm memCreateForm, Model model) {
 		String employeeId = memService.createEmployeeId(memCreateForm.getDeptName(), memCreateForm.getStartDate());
 		memCreateForm.setEmployeeId(employeeId); // 사원 번호를 폼 객체에 설정
-	    memService.save(memCreateForm); // 사원 정보 저장
+		memService.save(memCreateForm); // 사원 정보 저장
 		return "redirect:/HR/mem/list";
 	}
-	
+
+	@GetMapping("/mem/search") // 매개변수가 선택적임
+	public String search(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
+		if (keyword != null && !keyword.isEmpty()) {
+			List<HR_mem> searchResults = memService.search(keyword);
+			model.addAttribute("searchResults", searchResults);
+		}
+		return "HR_memSearch";
+	}
 
 }
-
--- JM_ERP/src/main/java/com/example/demo/Repository/HR_memRepository.java
+++ JM_ERP/src/main/java/com/example/demo/Repository/HR_memRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.example.demo.Entity.HR_mem;
 
-public interface HR_memRepository extends JpaRepository<HR_mem, String>{
+public interface HR_memRepository extends JpaRepository<HR_mem, String>, JpaSpecificationExecutor<HR_mem>{
 
 }
