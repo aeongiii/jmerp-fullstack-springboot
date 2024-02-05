8bdf8169bf05726e30902febb05864d4546b1a0e
skshrnrdj <skshrnrdj@gmail.com>
2024-02-05 16:27:21 +0900

    url change and href define

--- JM_ERP/src/main/java/com/example/demo/Controller/AC_FinanceController.java
+++ JM_ERP/src/main/java/com/example/demo/Controller/AC_FinanceController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.Entity.AC_Month;
 import com.example.demo.Service.AC_MonthService;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/AC")
 @RequiredArgsConstructor
 @Controller
 public class AC_FinanceController {
