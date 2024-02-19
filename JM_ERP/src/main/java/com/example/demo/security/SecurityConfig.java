f3be7fe8a9b000f335e4a427d6ebd0e95a958182
CheonJunHo <cdj8458@naver.com>
2024-02-19 17:45:31 +0900

    24-02-19 시큐리티 문제 해결

--- JM_ERP/src/main/java/com/example/demo/security/SecurityConfig.java
+++ JM_ERP/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -17,37 +17,35 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(
-				(authorizeHttpRequests) -> authorizeHttpRequests
-						/* .requestMatchers(new AntPathRequestMatcher("/user/login")).permitAll() */
-						.requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
-		/*
-		 * .requestMatchers(new AntPathRequestMatcher("/user/signup")).permitAll()
-		 * .requestMatchers(new AntPathRequestMatcher("/bootstrapLux.css")).permitAll()
-		 * .requestMatchers(new AntPathRequestMatcher("/error/**")).permitAll()
-		 * .requestMatchers(new AntPathRequestMatcher(
-		 * "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-		 * )) .permitAll() .requestMatchers(new
-		 * AntPathRequestMatcher("/")).authenticated() .requestMatchers(new
-		 * AntPathRequestMatcher("/user/**")).authenticated() // 전체 공개
-		 * 
-		 * 
-		 * .requestMatchers(new
-		 * AntPathRequestMatcher("/PC/**")).hasAnyAuthority("ROLE_구매")
-		 * .requestMatchers(new
-		 * AntPathRequestMatcher("/SD/**")).hasAnyAuthority("ROLE_영업")
-		 * .requestMatchers(new
-		 * AntPathRequestMatcher("/PD/**")).hasAnyAuthority("ROLE_생산")
-		 * .requestMatchers(new
-		 * AntPathRequestMatcher("/AC/**")).hasAnyAuthority("ROLE_회계")
-		 * .requestMatchers(new
-		 * AntPathRequestMatcher("/MG/**")).hasAnyAuthority("ROLE_재고")
-		 * .requestMatchers(new
-		 * AntPathRequestMatcher("/HR/**")).hasAnyAuthority("ROLE_인사") //TODO 관리자 권한
-		 * 취가해야함 밥
-		 */						)
+		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				.requestMatchers(new AntPathRequestMatcher("/user/login")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
 
-  	.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")).disable())
+				.requestMatchers(new AntPathRequestMatcher("/user/signup")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/bootstrapLux.css")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/style.css")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/calendar.css")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/calendar.js")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/bootstrapMorph.css")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/dataTable.css")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/sidebar.css")).permitAll()
+				
+				.requestMatchers(new AntPathRequestMatcher("/error/**")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher(
+						"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"))
+				.permitAll().requestMatchers(new AntPathRequestMatcher("/")).authenticated()
+				.requestMatchers(new AntPathRequestMatcher("/user/**")).authenticated() // 전체 공개
+
+				.requestMatchers(new AntPathRequestMatcher("/PC/**")).hasAnyAuthority("ROLE_구매")
+				.requestMatchers(new AntPathRequestMatcher("/SD/**")).hasAnyAuthority("ROLE_영업")
+				.requestMatchers(new AntPathRequestMatcher("/PD/**")).hasAnyAuthority("ROLE_생산")
+				.requestMatchers(new AntPathRequestMatcher("/AC/**")).hasAnyAuthority("ROLE_회계")
+				.requestMatchers(new AntPathRequestMatcher("/MG/**")).hasAnyAuthority("ROLE_구매")
+				.requestMatchers(new AntPathRequestMatcher("/HR/**")).hasAnyAuthority("ROLE_인사") // TODO 관리자 권한
+
+		)
+
+				.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")).disable())
 
 				.headers((headers) -> headers.addHeaderWriter(
 						new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
