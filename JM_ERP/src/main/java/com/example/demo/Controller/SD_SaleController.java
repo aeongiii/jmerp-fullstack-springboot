18f0ef7e56b06fb6605e6ac0ce1f6fa543ade4d1
aeongiii <151815152+aeongiii@users.noreply.github.com>
2024-03-08 16:49:39 +0900

    영업관리 - 1, 2 완료

--- JM_ERP/src/main/java/com/example/demo/Controller/SD_SaleController.java
+++ JM_ERP/src/main/java/com/example/demo/Controller/SD_SaleController.java
@@ -1,31 +1,78 @@
 package com.example.demo.Controller;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.Service.SD_PurchaseService;
 import com.example.demo.Service.SD_SaleService;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Controller	
+@Controller
 @RequestMapping("/SD/sale")
 public class SD_SaleController {
-	
+
 	@Autowired
-	private final SD_PurchaseService purchaseService;
 	private final SD_SaleService saleService;
+	
+// 카테고리별 총 판매량 비교
+	@GetMapping("/nb")
+	public String nbGraph(Model model) {
+		
+		String categoryName = "공구,도서,리빙,식품,전자기기,패션";
+		String imagePath = "src/main/resources/static/img/SD_graphNB.png";	// 이미지 이름 변경하면서 imageName 안 쓰게 됨
+        String imageWebPath = "/img/SD_graphNB.png";
+		
+		// 이미지 존재하지 않을 경우, 그래프 생성 로직 실행
+		 if (!Files.exists(Paths.get(imagePath))) {
+             // 이미지가 존재하지 않는 경우, 그래프 생성 로직 실행
+             imageWebPath = saleService.graph_nb(categoryName);
+         }
+         // 모델에 그래프 추가
+         model.addAttribute("SD_graphNB", imageWebPath);
+		return "SD/SD_saleNB";
+	}
+	
+// 자체제작 상품별 판매량 비교	
+	
+	
+	
+	
 
+// 카테고리 내 상품별 판매량 비교
+	@GetMapping("/category")
+	public String categoryGraph(Model model) {
 
-// 인기상품 그래프 출력할 화면   
-	@GetMapping("/best")
-	public String bestGraph(Model model) {
-		String graphHtmlPath = saleService.executePythonScript();
-        model.addAttribute("graphHtmlPath", graphHtmlPath);
-		return "SD/SD_best";
+		// 카테고리 + 이미지 파일명 매핑
+		Map<String, String> categoryMap = new HashMap<>();
+		categoryMap.put("공구", "SD_gonggu");
+		categoryMap.put("도서", "SD_book");
+		categoryMap.put("리빙", "SD_living");
+		categoryMap.put("식품", "SD_food");
+		categoryMap.put("전자기기", "SD_electronic");
+		categoryMap.put("패션", "SD_fashion");
+		
+		// 카테고리별 이미지 유무 확인
+		categoryMap.forEach((categoryName, imageName) -> {
+            String imagePath = "src/main/resources/static/img/SD_" + categoryName + ".png";	// 이미지 이름 변경하면서 imageName 안 쓰게 됨
+            String imageWebPath = "/img/SD_" + categoryName + ".png";
+
+            if (!Files.exists(Paths.get(imagePath))) {
+                // 이미지가 존재하지 않는 경우, 그래프 생성 로직 실행
+                imageWebPath = saleService.graph_category(imageName, categoryName);
+            }
+            // 모델에 그래프 추가
+            model.addAttribute("SD_" + categoryName, imageWebPath);
+        });
+		
+		return "SD/SD_saleCategory";
 	}
 }
--- JM_ERP/src/main/java/com/example/demo/Service/SD_SaleService.java
+++ JM_ERP/src/main/java/com/example/demo/Service/SD_SaleService.java
@@ -7,10 +7,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Function;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.Entity.SD_NBProduct;
 import com.example.demo.Entity.SD_Purchase;
+import com.example.demo.Repository.SD_NBProductRepository;
 import com.example.demo.Repository.SD_PurchaseRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -22,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class SD_SaleService {
 
 	private final SD_PurchaseRepository purchaseRepository;
+	private final SD_NBProductRepository nbRepository;
 	private final ObjectMapper objectMapper;
 
 	public List<SD_Purchase> getList() {
@@ -29,20 +33,24 @@ public class SD_SaleService {
 	}
 
 	// 파이썬과 스프링부트를 연결
-	public String executePythonScript() {
-		String outputPath = "src/main/resources/static/img/SD_output.png"; // 그래프를 이미지(SD_output)로 저장할 파일 경로
-		try {
 
-			// SD_Purchase 데이터를 JSON 문자열 데이터로 변환
-			String jsonData = getSDPurchaseDataAsJson();
+// 카테고리별 총 판매량 비교
+	public String graph_nb(String categoryName) {
+		
+		String outputPath = "src/main/resources/static/img/SD_graphNB.png"; // 그래프를 이미지로 저장할 파일 경로
+		
+		try {
+			
+			// SD_NBProduct 데이터를 JSON 문자열 데이터로 변환
+			String jsonData = changeToJson(categoryName);
 
 			// JSON 데이터를 임시 파일에 저장
 			Path tempFile = Files.createTempFile(null, ".json");
 			Files.write(tempFile, jsonData.getBytes(StandardCharsets.UTF_8)); // CP949 오류 있어서 UTF-8로 변경
 
-			// 임시 파일의 경로를 파이썬 스크립트에 인자로 전달 // 파이썬 파일 경로 지정 // 인자 1 // 인자 2
-			ProcessBuilder processBuilder = new ProcessBuilder("python", "python/purchase.py", tempFile.toString(),
-					outputPath);
+			// 임시 파일의 경로를 파이썬 스크립트에 인자로 전달 					// 파이썬 파일 경로 지정	    // 인자 1 // 인자 2
+			ProcessBuilder processBuilder = new ProcessBuilder("python", "python/graphNB.py", tempFile.toString(),
+					outputPath, categoryName);
 			Process process = processBuilder.start();
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -69,8 +77,9 @@ public class SD_SaleService {
 
 			// 임시 파일 삭제하는게 깔끔
 			Files.delete(tempFile);
-
-			return "/img/SD_output.png"; // 그래프 이미지 파일 경로 반환
+			
+			
+			return "/img/SD_graphNB.png"; // 그래프 이미지 파일 경로 반환
 
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
@@ -78,15 +87,68 @@ public class SD_SaleService {
 		}
 	}
 
-	// SD_Purchase 데이터를 조회하고 JSON 문자열로 변환
-	public String getSDPurchaseDataAsJson() {
+// '공구'카테고리 상품 막대그래프로 출력			// imageName = "SD_gonggu", categoryName = "공구"
+	public String graph_category(String imageName, String categoryName) {
+		String outputPath = String.format("src/main/resources/static/img/SD_%s.png", categoryName); // 그래프를 이미지(SD_공구)로 저장할 파일 경로
 		try {
-			List<SD_Purchase> purchases = this.getList(); // 모든 구매 데이터 조회
-			return objectMapper.writeValueAsString(purchases); // JSON 문자열로 변환
+			
+			// SD_NBProduct 데이터를 JSON 문자열 데이터로 변환
+			String jsonData = changeToJson(categoryName);
+
+			// JSON 데이터를 임시 파일에 저장
+			Path tempFile = Files.createTempFile(null, ".json");
+			Files.write(tempFile, jsonData.getBytes(StandardCharsets.UTF_8)); // CP949 오류 있어서 UTF-8로 변경
+
+			// 임시 파일의 경로를 파이썬 스크립트에 인자로 전달 							// 파이썬 파일 경로 지정	    // 인자 1~3
+			ProcessBuilder processBuilder = new ProcessBuilder("python", "python/graphCategory.py", tempFile.toString(),
+					outputPath, categoryName);
+			Process process = processBuilder.start();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			// 표준 출력 읽기 : 로그에서 오류 확인용
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream())); // 표준 오류 스트림
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String s;
+			while ((s = stdInput.readLine()) != null) {
+				System.out.println(s);
+			}
+			// 표준 오류 출력 읽기 : 로그에서 오류 확인용
+			while ((s = stdError.readLine()) != null) {
+				System.err.println(s);
+			}
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			int exitCode = process.waitFor();
+			System.out.println("\nExited with error code : " + exitCode);
+
+			// 임시 파일 삭제하는게 깔끔
+			Files.delete(tempFile);
+			
+			
+			return String.format("/img/SD_%s.png", categoryName); // 그래프 이미지 파일 경로 반환
+
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// 매개변수에 따른 카테고리 데이터를 조회하고 JSON 문자열로 변환
+	public String changeToJson(String categoryName) {
+		try {
+//			List<SD_NBProduct> categoryList = nbRepository.findByCategory(categoryName); // 해당 카테고리에 포함된 모든 상품 데이터 조회
+			List<SD_NBProduct> categoryList = nbRepository.findAll(); // 모든 상품 데이터 조회  -> 파이썬에서 카테고리별로 걸러서 사용함.
+			return objectMapper.writeValueAsString(categoryList); // JSON 문자열로 변환
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null; // 오류 발생 시 null 반환 또는 적절한 예외 처리
 		}
 	}
+	
 
 }
